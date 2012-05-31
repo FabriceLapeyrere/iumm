cd94720c4276be348cf5d7d8b68a1d05bf31927b
FabriceLapeyrere <fabrice.lapeyrere@surlefil.org>
2012-05-31 10:07:07 +0200

    debut de travail sur l'interface d'admin

---
+++ ctl/init/utilisateurs.sqlite.sql
@@ -0,0 +1,3 @@
+CREATE TABLE utilisateurs (rowid INTEGER PRIMARY KEY AUTOINCREMENT, nom , login , mdp , date DEFAULT (CURRENT_TIMESTAMP));
+INSERT INTO utilisateurs (nom,login,mdp) VALUES ('demo', 'demo', 'ke00zyBQaBhcw');
+
